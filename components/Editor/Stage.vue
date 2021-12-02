4c3971a0000b6952f2b94d2b7be465339cef0b60
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-12-02 00:00:57 -0300

    Implemented editor zoom

--- components/Editor/Stage.vue
+++ components/Editor/Stage.vue
@@ -8,6 +8,7 @@
     @click="handleClick"
     @dragmove="handleDragMove"
     @dragend="handleDragEnd"
+    @wheel="handleMouseWheel"
   >
     <!-- LAYER 1: ENTITIES -->
     <v-layer>
@@ -165,6 +166,41 @@ export default {
     handleDragEnd (e) {
       this.commitEntityMovement()
       this.currentEntity.id = null
+    },
+    handleMouseWheel (e) {
+      // Based on: https://konvajs.org/docs/sandbox/Zooming_Relative_To_Pointer.html
+
+      // Applies zoom as the user scrolls
+
+      const scaleBy = 1.1
+      const stage = this.$refs.stage.getNode()
+      const oldScale = stage.scaleX()
+      const pointer = stage.getPointerPosition()
+
+      const mousePointTo = {
+        x: (pointer.x - stage.x()) / oldScale,
+        y: (pointer.y - stage.y()) / oldScale
+      }
+
+      // how to scale? Zoom in? Or zoom out?
+      let direction = e.evt.deltaY > 0 ? -1 : 1
+
+      // when we zoom on trackpad, e.evt.ctrlKey is true
+      // in that case lets revert direction
+      if (e.evt.ctrlKey) {
+        direction = -direction
+      }
+
+      const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy
+
+      stage.scale({ x: newScale, y: newScale })
+
+      const newPos = {
+        x: pointer.x - mousePointTo.x * newScale,
+        y: pointer.y - mousePointTo.y * newScale
+      }
+
+      stage.position(newPos)
     }
   }
 }
