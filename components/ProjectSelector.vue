16fa21dcdfb65e171b73ead2f39ed35c8d1eb627
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-12-15 13:48:48 -0300

    Implemented project search UI

--- components/ProjectSelector.vue
+++ components/ProjectSelector.vue
@@ -6,69 +6,101 @@
         Projetos recentes
       </span>
       <TextSeparator />
-      <a href="#">
-        Pesquisar projeto
+      <a @click="showingSearch = !showingSearch">
+        <span v-if="showingSearch">
+          Cancelar
+        </span>
+        <span v-else>
+          Pesquisar projeto
+        </span>
       </a>
     </v-system-bar>
-    <v-container class="d-flex justify-center">
-      <v-menu
-        v-for="project in projects"
-        :key="project.id"
-        v-model="showingMenuProjects[project.id]"
-        offset-y
-        :close-on-content-click="false"
-      >
-        <template #activator="{ on, attrs }">
-          <AvatarCard
-            :title="project.name"
-            :highlight="project.id === value"
-            v-bind="attrs"
-            v-on="(
-              project.isAllowed($auth.user.sub, [AccessLevel.OWNER]) &&
-              project.id === value
-            ) ? on : null"
-            @click="() => $emit('input', project.id)"
-          >
-            <v-icon size="32">
-              mdi-notebook
-            </v-icon>
-          </AvatarCard>
-        </template>
-        <UpdateProjectCard
-          :entity-id="project.id"
-          @response="showingMenuProjects[project.id] = false"
+    <v-row
+      v-if="showingSearch"
+      class="d-flex justify-center align-center"
+    >
+      <v-col cols="12" sm="10" md="8" lg="6" xl="4">
+        <v-text-field
+          solo
+          full-width
+          hide-details
+          autofocus
+          prepend-inner-icon="mdi-magnify"
+          label="Insira o nome de um projeto para pesquisar"
+          @keydown.esc="showingSearch = false"
         />
-      </v-menu>
-      <v-menu
-        v-model="showingMenuCreateProject"
-        offset-y
-        :close-on-content-click="false"
-      >
-        <template #activator="{ on, attrs }">
-          <AvatarCard
-            title="Criar Projeto"
-            v-bind="attrs"
-            v-on="on"
+      </v-col>
+    </v-row>
+    <v-row class="d-flex justify-center align-center">
+      <v-col cols="12" sm="10" xl="7">
+        <v-container class="d-flex justify-center flex-wrap">
+          <v-menu
+            v-for="project in projects"
+            :key="project.id"
+            v-model="showingMenuProjects[project.id]"
+            offset-y
+            :close-on-content-click="false"
           >
-            <v-icon>
-              mdi-plus
-            </v-icon>
-          </AvatarCard>
-        </template>
-        <CreateProjectCard
-          @created="response => {
-            showingMenuCreateProject = false
-            $emit('input', response.data.id)
-          }"
-        />
-      </v-menu>
-    </v-container>
+            <template #activator="{ on, attrs }">
+              <AvatarCard
+                :title="project.name"
+                :highlight="project.id === value"
+                v-bind="attrs"
+                v-on="(
+                  project.isAllowed($auth.user.sub, [AccessLevel.OWNER]) &&
+                  project.id === value
+                ) ? on : null"
+                @click="() => {
+                  $emit('input', project.id); showingSearch = false
+                }"
+              >
+                <v-icon size="32">
+                  mdi-notebook
+                </v-icon>
+              </AvatarCard>
+            </template>
+            <UpdateProjectCard
+              :entity-id="project.id"
+              @response="showingMenuProjects[project.id] = false"
+            />
+          </v-menu>
+          <v-menu
+            v-if="!showingSearch"
+            v-model="showingMenuCreateProject"
+            offset-y
+            :close-on-content-click="false"
+          >
+            <template #activator="{ on, attrs }">
+              <AvatarCard
+                title="Criar Projeto"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>
+                  mdi-plus
+                </v-icon>
+              </AvatarCard>
+            </template>
+            <CreateProjectCard
+              @created="response => {
+                showingMenuCreateProject = false
+                $emit('input', response.data.id)
+              }"
+            />
+          </v-menu>
+        </v-container>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import { AccessLevel } from '~/models/enum/access-level'
 import { Project } from '~/models/project'
+
+const PROJECT_COUNT_SEARCH = 12
+const PROJECT_COUNT_LIST = 4
+
 export default {
   props: {
     value: {
@@ -80,6 +112,7 @@ export default {
     return {
       showingMenuProjects: {},
       showingMenuCreateProject: false,
+      showingSearch: false,
       AccessLevel
     }
   },
@@ -87,8 +120,12 @@ export default {
     allProjects () {
       return Project
         .query()
-        .orderBy('id')
-        .limit(6) // Max amount of projects to display
+        .orderBy('id', 'desc')
+        .limit(this.showingSearch
+          ? PROJECT_COUNT_SEARCH
+          : PROJECT_COUNT_LIST
+        )
+        // Max amount of projects to display
         // To see more you must search
         .get()
     },
@@ -104,7 +141,12 @@ export default {
   created () {
     Project
       .api()
-      .get(Project.entity, { dataKey: 'items' })
+      .get(Project.entity, {
+        query: {
+          limit: PROJECT_COUNT_SEARCH
+        },
+        dataKey: 'items'
+      })
       .then(() => {
         if (this.projects.length > 0) {
           this.$emit('input', this.projects[0].id)
